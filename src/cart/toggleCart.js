a00473d0736033c7f7bf305245d9f730c69c6820
ShiroganeIXV <wudiana.124010121105@vtc.edu.vn>
2024-04-28 16:30:24 +0700

    add toggle-cart

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,20 @@
 import { getElement } from '../utils.js';
 
-export const openCart = () => {};
+//select elements
+const cartOverlay = getElement('.cart-overlay');
+const closeCartBtn = getElement('.cart-close');
+const toggleCartBtn = getElement('.toggle-cart ');
+
+// add event listeners
+toggleCartBtn.addEventListener('click', () => {
+  cartOverlay.classList.add('show');
+});
+
+closeCartBtn.addEventListener('click', () => {
+  cartOverlay.classList.remove('show');
+});
+
+// when add item to cart
+export const openCart = () => {
+    cartOverlay.classList.add('show');
+};
